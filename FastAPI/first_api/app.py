2e75dac4ec1b3e88b3229c00cbc6573ef23b4bb0
Nidhish Chauhan <84083897+Nidhish-07@users.noreply.github.com>
2023-10-08 16:05:43 +0000

    post endpoint added

--- FastAPI/first_api/app.py
+++ FastAPI/first_api/app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
 app = FastAPI()
 
-BOOKS = [{"Title": "Rachel Wilkerson", "Author": "Raya Burton", "Genre": "Priscilla Newton"}, {"Title": "Libby Hardy", "Author": "Caldwell Burch", "Genre": "Althea Conley"}, {"Title": "Lana Whitehead","Author": "Beau Solomon", "Genre": "Shellie Mcleod"}, {"Title": "Naomi Murray", "Author": "Adam Ayala", "Genre": "Kessie Haney"}, {"Title": "Tamekah Dunlap", "Author": "Haviva Roy", "Genre": "Piper Mooney"}]
+BOOKS = [{"Title": "Rachel Wilkerson", "Author": "Raya Burton", "Genre": "Action"}, {"Title": "Libby Hardy", "Author": "Caldwell Burch", "Genre": "Action"}, {"Title": "Lana Whitehead","Author": "Beau Solomon", "Genre": "Adventure"}, {"Title": "Naomi Murray", "Author": "Adam Ayala", "Genre": "Adventure"}, {"Title": "Tamekah Dunlap", "Author": "Haviva Roy", "Genre": "Adventure"}]
 
+class Book(BaseModel):
+    Title:str
+    Author:str
+    Genre:str
 
 @app.get("/")
 async def home():
@@ -14,3 +21,25 @@ async def get_book(title: str):
         if book.get('Title').casefold()==title.casefold():
             return book
         
+
+@app.get("/books")
+async def get_books_by_category(category: str):
+    books_by_category=[]
+    for book in BOOKS:
+        if book.get("Genre").casefold()==category.casefold():
+            books_by_category.append(book)
+    
+    return books_by_category
+
+@app.get("/books/{author}")
+async def get_books_by_author_and_category(author: str, category: str):
+    books=[]
+    for book in BOOKS:
+        if book.get("Author").casefold()==author.casefold() and book.get("Genre").casefold()==category.casefold():
+            books.append(book)
+            
+    return books
+
+@app.post("/books/create_book")
+async def create_book(new_book:Book):
+    BOOKS.append(new_book)
